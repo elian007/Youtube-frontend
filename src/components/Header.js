da72f9cdd10e45dd2cbeec385a9f8c2e2a9c318b
Elian07 <elians.2016@alunos.utfpr.edu.br>
2019-11-23 12:46:13 -0300

    Arrumando search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,25 +6,26 @@ import './Header.css'
 import logo from '../assets/logo.svg'
 import upload from '../assets/upload.svg'
 
+
+
+
 export default function Header() {
+ 
   return (
     <header id='main-header'>
         <div className='header-content'>
             <Link to='/'>
                 <img src={logo} className='img1'alt='Videos' />
             </Link>
-            <Link to='/upload'>
-                <span><img src={upload} className='img2' alt='Uplaod de vídeos'/></span>
-            </Link>
-            
-            <form>
-              <label>Pesquisar</label>
-              <input type='text' class='form-control' placeholder='Pesquisar vídeo...' />
+
+            <input type='text' class='form-control' placeholder='Pesquisar vídeo...' />
               <Link to='/pesquisa'>
-              <button type='submit' class='btn-btn-primary'>Enviar</button>
+                <button type='submit' class='btn-btn-primary'>Enviar</button>
               </Link>
-            </form>
           
+            <Link to='/upload'>
+              <span><img src={upload} className='img2' alt='Uplaod de vídeos'/></span>
+            </Link>
         </div>
     </header>
   );
