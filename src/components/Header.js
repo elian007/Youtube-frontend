674ead9dbd9c839650be25c079def72477ac8622
Elian07 <elians.2016@alunos.utfpr.edu.br>
2019-11-26 10:59:43 -0300

    Arrumando nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-
-import './Header.css'
-
 import logo from '../assets/logo.svg'
 import upload from '../assets/upload.svg'
+import './Header.css'
 
 
+export default class Header extends Component{
 
 
-export default function Header() {
- 
-  return (
-    <header id='main-header'>
-        <div className='header-content'>
-            <Link to='/'>
-                <img src={logo} className='img1'alt='Videos' />
-            </Link>
+    render(){
 
-            <input name='pesquisar'type='text' class='form-control' placeholder='Pesquisar vídeo...' />
-              <Link to='/pesquisa'>
-                <button type='submit' class='btn-btn-primary'>Enviar</button>
-              </Link>
+
+        return(
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <header>
+                <Link to='/'>
+                    <img src={logo} className='img1'alt='Videos' />
+                </Link>
           
-            <Link to='/upload'>
-              <span><img src={upload} className='img2' alt='Uplaod de vídeos'/></span>
-            </Link>
-        </div>
-    </header>
-  );
+                
+                    
+                    
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <Link to='/upload'>
+                    <span><img src={upload} className='img2' alt='Uplaod de vídeos'/></span>
+                </Link>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+                </header>
+            </nav>
+        )
+    }
+
+
 }
